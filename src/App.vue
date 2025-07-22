abab941d7e530f97de6d5dd2cb0d57294b3e882f
ljt990218 <2511590093@qq.com>
2025-07-22 18:29:07 +0800

    fix: 修改单位选择为 rpx，并添加输出结果自动复制到剪贴板的提示

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { toUnocssClass } from './utils/index.js'
 
 const inputText = ref(`width: 470rpx; height: 172rpx; background: #FFFFFF; border-radius: 16rpx; `)
 const outputText = ref([]) as any
-const removeUnit = ref('px')
+const removeUnit = ref('rpx')
 
 const handleInput = () => {
   const removePxVal = removeUnit.value === 'px'
@@ -31,6 +31,7 @@ handleInput()
     <label for="removePx">去掉 px 单位</label>
     <input type="radio" id="removeRpx" value="rpx" v-model="removeUnit" name="removeUnit" />
     <label for="removeRpx">去掉 rpx 单位</label>
+    <span class="text-sm">（输出的结果自动复制到剪贴板）</span>
   </div>
 
   <div class="w-60vw h-full flex gap-4 mt-2">
